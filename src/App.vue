2666d5700b6e280767a1be4831cc8b029bb56e0e
baymax44 <smartbay2021@outlook.com>
2020-07-02 01:54:27 -0400

    first component & play with first app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Modal
+  },
+  data() {
+    return {
+      title: "My first vue app :)"
+    }
+  },
 }
 </script>
 
@@ -22,5 +27,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #eee;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
